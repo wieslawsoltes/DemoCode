b6c80e5518ede37974c0f70be327ca0da4b781fb
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-04-20 14:53:11 +0200

    Use GroupBox control instead of Expander

---
+++ Drums/VDrumExplorer.Gui.Avalonia/Controls/GroupBox.axaml.cs
@@ -0,0 +1,17 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+
+namespace VDrumExplorer.Gui.Controls;
+
+public class GroupBox : ContentControl
+{
+    public static readonly StyledProperty<object> HeaderProperty =
+        AvaloniaProperty.Register<GroupBox, object>(nameof(Header));
+
+    public object Header
+    {
+        get => GetValue(HeaderProperty);
+        set => SetValue(HeaderProperty, value);
+    }
+}
